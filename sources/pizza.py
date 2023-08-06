886cdddf1426ac7b28472e43b19c4613b526055a
Neo <dam.workit40@gmail.com>
2023-08-06 07:08:47 +0200

    add getsuga pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,10 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'getsuga': {
+        'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
+        'price': 11,
     }
+
 }
